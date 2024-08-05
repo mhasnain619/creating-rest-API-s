23c413e2c81c0b2a18287a65b5b16359eea158c8
Muhammad Hasnain <hasnain0893@gmail.com>
2024-08-05 23:02:27 +0500

    creating apis

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-const express = require()
-
+const express = require("express")
+const users = require("./MOCK_DATA.json")
 const app = express()
 const PORT = 8000
 
 //ROUTES
+app.get('/users', (req, res) => {
+
+    const html = `
+    <ul>
+    
+    ${users.map(user => `<li>${user.first_name}</li>`).join("")}
+    
+    </ul>`;
+    res.send(html)
+
+})
+
+//Rest Api
+
+app.get('/api/users', (req, res) => {
+    return res.json(users)
+})
+app.get('/api/users/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const user = users.find((user) => user.id === id)
+    return res.json(user)
+})
 
 
 app.listen(PORT, () => console.log(`Server Startwd at PORT:${PORT}`));
