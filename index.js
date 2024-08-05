89a8337d0e831d8fa8f067c8b4f52a9196475c34
Muhammad Hasnain <hasnain0893@gmail.com>
2024-08-05 23:22:09 +0500

    Api's Creaated

--- index.js
+++ index.js
@@ -17,15 +17,58 @@ app.get('/users', (req, res) => {
 })
 
 //Rest Api
+// Getting all users
 
 app.get('/api/users', (req, res) => {
     return res.json(users)
 })
-app.get('/api/users/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const user = users.find((user) => user.id === id)
-    return res.json(user)
+
+app.route('/api/users/:id').get(
+    (req, res) => {
+        const id = Number(req.params.id)
+        const user = users.find((user) => user.id === id)
+        res.json(user)
+    })
+    .patch((req, res) => {
+        // Edit users by id 
+        return res.json({ status: pending })
+    })
+    .delete((req, res) => {
+        // Delete users by id 
+        return res.json({ status: pending })
+    })
+
+
+// Creating user
+
+app.post('/api/users', (req, res) => {
+    //Todo Create new user
+    return res.send({ status: pending })
 })
 
+
+// Getting users by id 
+
+// app.get('/api/users/:id', (req, res) => {
+//     const id = Number(req.params.id)
+//     const user = users.find((user) => user.id === id)
+//     return res.json(user)
+// })
+
+
+
+//Update User
+
+// app.patch('/api/users/:id', (req, res) => {
+//     //Todo Edit new user with id
+//     return res.send({ status: pending })
+// })
+
+//Delete User
+
+// app.delete('/api/users/:id', (req, res) => {
+//     //Todo delete new user with id
+//     return res.send({ status: pending })
+// })
 
 app.listen(PORT, () => console.log(`Server Startwd at PORT:${PORT}`));
